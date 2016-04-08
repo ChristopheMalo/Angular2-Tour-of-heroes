2fb20ccb71e9647101b482eb6ac592533e74b172
ChristopheMalo <christof.malo@gmail.com>
2016-04-08 10:02:19 +0200

    feat: inti files with some codes to display simple h1/h2 and vars

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,1 +1,11 @@
+import {Component} from 'angular2/core';
 
+@Component({
+  selector: 'hero-app',
+  template: '<h1>{{titile}}</h1><h2>{{hero}} details</h2>'
+})
+
+export class AppComponent {
+  title = 'Tour of heroes';
+  hero  = 'Magneto';
+}
