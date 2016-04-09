ca2b3c7d6051504aba6b9019bba13f1fea484406
ChristopheMalo <christof.malo@gmail.com>
2016-04-09 19:14:56 +0200

    feat: code the class Hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,14 +7,31 @@
 // Import library
 import {Component} from 'angular2/core';
 
+// Hero class
+export class Hero {
+  id:   number;
+  name: string;
+}
+
 // The decorator - What template use end hox to create the component
 @Component({
   selector: 'hero-app',
-  template: '<h1>{{titile}}</h1><h2>{{hero}} details</h2>'
+  template: `
+            <h1>{{title}}</h1>
+            <h2>{{hero.name}} details</h2>
+            <div><label>id: </label>{{hero.id}}</div>
+            <div><label>name: </label><input [(ngModel)]="hero.name" placeholder="name"></div>
+            `
 })
 
 // The class controls the appearance and behavior of view through its template
 export class AppComponent {
+  // Titile of the page
   title = 'Tour of heroes';
-  hero  = 'Magneto';
+  
+  // Hero property
+  hero: Hero = {
+    id: 1,
+    name: 'Magneto'
+  };
 }
