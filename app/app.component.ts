faf08ced4efacdba0d41f280822e2fd6f64d3952
ChristopheMalo <christof.malo@gmail.com>
2016-04-10 17:11:31 +0200

    feat: style the heroes list

--- app/app.component.ts
+++ app/app.component.ts
@@ -18,10 +18,65 @@ export class Hero {
   selector: 'hero-app',
   template: `
             <h1>{{title}}</h1>
+            <h2>My Heroes</h2>
+            <ul class="heroes">
+              <li *ngFor="#hero of heroes">
+                <span class="badge">{{hero.id}}</span>: {{hero.name}}
+              </li>
+            </ul>
             <h2>{{hero.name}} details</h2>
             <div><label>id: </label>{{hero.id}}</div>
             <div><label>name: </label><input [(ngModel)]="hero.name" placeholder="name"></div>
-            `
+            `,
+  styles:[`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
 
 // The class controls the appearance and behavior of view through its template
@@ -29,9 +84,26 @@ export class AppComponent {
   // Titile of the page
   title = 'Tour of heroes';
   
+  // Property for the heroes
+  heroes = HEROES;
+  
   // Hero property
   hero: Hero = {
     id: 1,
     name: 'Magneto'
   };
 }
+
+// An array of heroes - type Hero
+var HEROES: Hero[] = [
+  {"id":11, "name": "Mr ChipMunks"},
+  {"id":12, "name": "Bombas"},
+  {"id":13, "name": "Mr Stone"},
+  {"id":14, "name": "James Bond"},
+  {"id":15, "name": "Spiderman"},
+  {"id":16, "name": "Tornado"},
+  {"id":17, "name": "The metal Surfer"},
+  {"id":18, "name": "Dr Who"},
+  {"id":19, "name": "The RubberMan"},
+  {"id":20, "name": "Elastic Girl"},
+];
